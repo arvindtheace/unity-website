bcdcd54f76131cbbad3515be6325b1da43842714
Arvind <arvindtheace@gmail.com>
2024-05-14 21:08:46 +0530

    added configurable background to documentcard

--- components/DocumentCard.tsx
+++ components/DocumentCard.tsx
@@ -2,6 +2,7 @@ import { format } from 'date-fns';
 import { ArrowRight, Download } from 'lucide-react';
 import React from 'react';
 import styled from 'styled-components';
+import { usePathname } from 'next/navigation';
 
 interface DocumentCardProps {
   file: string;
@@ -13,9 +14,10 @@ interface DocumentCardProps {
 }
 
 const DocumentCard: React.FC<DocumentCardProps> = ({ file, type, link, title, date, description }) => {
+  const pathname = usePathname()
   return (
     <a href={type === "link" ? link : file} target="_blank" rel="noopener noreferrer" >
-      <PDFCard className="relative sm">
+      <PDFCard className="relative sm" bgColor={pathname?.startsWith('/business') ? '#D4D4D4': '#FFFEF9'}>
           <p>{title}</p>
           {description && (
             <div className="text-gray-500 text-sm mt-4">{description}</div>
@@ -34,11 +36,12 @@ const DocumentCard: React.FC<DocumentCardProps> = ({ file, type, link, title, da
 
 export default DocumentCard;
 
-const PDFCard = styled.div`
+const PDFCard = styled.div<{ bgColor: string }>`
   padding: 24px;
   border: 1px solid rgba(0, 0, 0, 0.16);
   border-radius: 16px;
   transition: all 0.3s ease;
+  background: ${({ bgColor }) => bgColor};
 
   & .icon {
     opacity: 0;
