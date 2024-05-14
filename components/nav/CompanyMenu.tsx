91fd4b633ba9eae59332a1cf731f45811600e077
Arvind <arvindtheace@gmail.com>
2024-05-14 20:25:16 +0530

    fixed navbar and added new hero images

--- components/nav/CompanyMenu.tsx
+++ components/nav/CompanyMenu.tsx
@@ -33,20 +33,11 @@ export const CompanyMenu = forwardRef<HTMLElement>((props, ref) => {
 })
 
 const StyledLink = styled.a`
-  &:hover {
-      a::before {
-      background: #FBF8EB;
-      width: 30px;
-      border-radius: 50%;
-      height: 30px;
-      positon: relative;
-      content: '';
-      margin-right: -27px;
-      opacity: 0.5;
-    }
+  &:hover p {
+    font-weight: bold;
   }
   &:hover p::after {
-    content: url('data:image/svg+xml, %3Csvg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"%3E%3Cpath d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"%3E%3C/path%3E%3C/svg%3E');
+    content: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none"%3E%3Cpath d="M12.6665 8L8.6665 12M12.6665 8L8.6665 4M12.6665 8L3.33317 8" stroke="black" style="stroke:black;stroke-opacity:1;" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/%3E%3C/svg%3E');
     position: relative;
     top: 2px;
     right: -5px;
